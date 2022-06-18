64eac487ddae417060860e56cd071e83b9f605ed
geljus <geljus@gmail.com>
2022-06-18 15:28:31 +0300

    Finished tasks

--- src/Arrays6.java
+++ src/Arrays6.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Arrays6 {
+import java.util.Scanner;
+
+/*Обработка данных массива
+Дано целое число N, не превосходящее 1000. Далее следуют N целых чисел.
+Среди всех чисел массива, необходимо вывести на экран двузначные, четные и положительные через пробел.
+А если таких чисел в массиве нет, необходимо вывести -1.*/
+
+public class Arrays6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int volume = in.nextInt();
+        int [] a = new int [volume];
+        for (int i =0; i<volume; i++){
+            a [i] = in.nextInt();
+        }
+        int count =0;
+        for (int i:a) {
+            if (i>9 && i<100 && (i % 2==0)) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+        if (count==0) System.out.print(-1);
+    }
 }
--- src/SumOfInteger.java
+++ src/SumOfInteger.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SumOfInteger {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumOfInteger {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        long sum=0;
+        for (int i=0; i<num; i++) {
+            sum=sum+(i+1);
+        }
+        System.out.println(sum);
+    }
 }
