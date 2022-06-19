937c575ac18774c1d1a04316018052e2d0547e7b
geljus <geljus@gmail.com>
2022-06-19 22:49:22 +0300

    Finished tasks

--- src/Arrays7.java
+++ src/Arrays7.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Arrays7 {
+/*Нарушенный порядок
+Дано целое число N, не превосходящее 1000. Далее следуют N целых чисел.
+Порядок нарушен тогда, когда среди двух соседних элементов массива левый элемент больше правого.
+Программа должна считать количество нарушенных порядков в массиве и выводить это количество на экран.*/
+
+import java.util.Scanner;
+
+public class Arrays7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int volume = in.nextInt();
+        int [] a = new int [volume];
+        for (int i =0; i<volume; i++){
+            a [i] = in.nextInt();
+        }
+        int count =0;
+        for (int i=0; i<(volume-1); i++) {
+            if (a[i]>a[i+1]) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
 }
+
